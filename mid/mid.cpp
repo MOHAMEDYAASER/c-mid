634f078839b33b01e9d981bd6625d926cbe17756
Mo <mo@gmail.com>
2024-03-24 22:26:10 +0200

    first commit

---
+++ mid/mid.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+using namespace std;
+int fact(int n) {
+	if (n <= 1) {
+		return 1;
+	}
+	else {
+		return n * fact(n - 1);
+	}
+}
+
+int main() {
+	cout << fact(1) << endl;
+}
